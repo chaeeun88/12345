25255affae9a1194974c31db1628ef8255505486
1233546 <127706200+chaeeun88@users.noreply.github.com>
2023-04-11 16:09:00 +0900

    Add files via upload

---
+++ ssss.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$n = 20;
+for($x = 0; $x < $n; $x++) {
+  $data[$x]=rand(0, 100);
+  echo "$data[$x] ";
+}
+echo "<br>";
+
+sort($data);
+
+for($y = 0; $y < $n; $y++) {
+  echo $data[$y];
+  echo "<br>";
+}
+
+?>
+
+</body>
+</html>
